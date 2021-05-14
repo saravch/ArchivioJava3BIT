77789449a0604e5708c37647f798eceff120e58b
Sara Vacher <Sara Vacher@Sara>
2021-05-14 19:15:30 +0200

    Aggiornamento del 14/05 Completato progetto archivio, problemi di stampa da risolvere

--- ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Archivio.java
@@ -1,6 +1,13 @@
 package archiviojava3bit;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Contiene gli oggetti del programma.
@@ -11,60 +18,194 @@ import java.util.ArrayList;
  * @author INSERISCI_NOME
  */
 public class Archivio {
-    private ArrayList<Articolo> archivio;
-    
-    //Costruttore 
-    public Archivio(){
-        //Creo l'ArrayList vuoto
-        archivio = new ArrayList(0);
+  // TO DO
+  // Aggiungere costante per il nome del file
+
+  private ArrayList<Articolo> archivio;
+
+  public Archivio() {
+    archivio = leggiDaFile();
+  }
+
+  // Metodo che restituisce l'elenco degli articoli
+  public ArrayList<Articolo> elencoArticoli() {
+    return archivio;
+  }
+
+  public boolean aggiungi(Articolo articolo) {
+    // controllo che non ci sia già un articolo con lo stesso 
+    // codice
+    if (Archivio.this.cercaArticolo(articolo.getCodice()) != null) {
+      return false;
     }
-    
-    //Metodo per inserire un Articolo
-    public void inserisci(Articolo articolo){
-        //Aggiungo l'oggetto ricevuto nell'ArrayList
-        archivio.add(articolo);
+
+    archivio.add(articolo);
+    salvaSuFile();
+    return true;
+  }
+
+  private Articolo cercaArticolo(String codice) {
+    for (int i = 0; i < archivio.size(); i++) {
+      if (archivio.get(i).getCodice().equals(codice)) {
+        return archivio.get(i);
+      }
     }
-    
-    //Metodo per la ricerca in base al codice
-    public Articolo ricerca(String codice){
-        for(int i = 0; i < archivio.size(); i++){
-            //Prendiamo l'oggetto in posizione i
-            Articolo articolo = archivio.get(i);
-            if(articolo.getCodice()==codice){
-                return articolo;
-            }
-        }
-        //Se arrivo qui significa che l'articolo non c'è
-        return null;
+    return null;
+  }
+
+  public boolean elimina(String codice) {
+    Articolo articolo;
+    // Ricerco l'articolo
+    articolo = Archivio.this.cercaArticolo(codice);
+
+    // Se l'articolo c'è lo cancello
+    if (articolo != null) {
+      // Cancello l'articolo dall'array list
+      archivio.remove(articolo);
+      //Salvo tutto nel file
+      salvaSuFile();
+      return true;
     }
-    
-    //Metodo che restituisce la stringa che rappresenta tutto l'archivio
-    public String toString(){
-        String s = "";
-        for(int i = 0; i < archivio.size(); i++){
-            Articolo articolo = archivio.get(i);
-            s += articolo.toString();
-            if(i < archivio.size() - 1){
-                s += "\r\n";
-            }
-        }
-        return s;
+    return false;
+  }
+
+  public boolean modificaArticolo(String nome, String codice, float prezzo, String descrizione, String colore) {
+
+    // Cerco l'articolo in base codice.
+    Articolo articolo;
+    articolo = Archivio.this.cercaArticolo(codice);
+    // Se è presente nell'archivio modifico le sue caratteristiche
+    if (articolo != null) {
+      articolo.setNome(nome);
+      articolo.setPrezzo(prezzo);
+      articolo.setDescrizione(descrizione);
+      articolo.setColore(colore);
+
+      // Salvo l'archivio su file
+      salvaSuFile();
+      return true;
+    } else {
+      return false;
     }
-    
-    //Metodo che restitiusce l'elenco degli articoli
-    public ArrayList<Articolo> elencoArticoli(){
-        return archivio;
+  }
+
+  public ArrayList<Articolo> cercaArticolo(float prezzoMinimo, float prezzoMassimo) {
+
+    // Parto con l'array list delle articolo trovate VUOTO
+    ArrayList<Articolo> articoliTrovati = new ArrayList(0);
+
+    for (int i = 0; i < archivio.size(); i++) {
+      if (archivio.get(i).getPrezzo() >= prezzoMinimo
+              && archivio.get(i).getPrezzo() <= prezzoMassimo) {
+        // Aggiungo l'articolo in posizione i nell'array list degli articoli trovati
+        articoliTrovati.add(archivio.get(i));
+      }
     }
-    
-    //Metodo per eliminare un articolo dall'archivio
-    public boolean elimina(String codice){
-        Articolo articolo;
-        articolo = ricerca(codice);
-        if(articolo != null){
-            //Cancello l'articolo dall'arraylist
-            archivio.remove(articolo);
-            return true;
-        }
-        return false;
+    return articoliTrovati;
+  }
+
+  // Metodo per cercare le articolo in base al modello
+  public ArrayList<Articolo> cercaArticoloPerDescrizione(String descrizione) {
+
+    // Parto con l'array list degli articoli trovati VUOTO
+    ArrayList<Articolo> articoliTrovati = new ArrayList(0);
+
+    // Converto la descrizione da cercare in minuscolo
+    String descrizioneDaCercareMinuscolo = descrizione.toLowerCase();
+
+    for (int i = 0; i < archivio.size(); i++) {
+      // Converto la descrizione dell'articolo in posizione i in minucolo
+      String descrizioneMinuscolo = archivio.get(i).getDescrizione().toLowerCase();
+
+      if (descrizioneMinuscolo.contains(descrizioneDaCercareMinuscolo)) {
+        // Aggiungo l'rticolo in posizione i nell'ArrayList degli
+        // articoli trovati.
+        articoliTrovati.add(archivio.get(i));
+      }
     }
+
+    // Restituisco l'ArrayList delle articolo trovate.
+    return articoliTrovati;
+  }
+
+  // Metodo che converte tutto l'arra list delle articolo in una stringa
+  public String toString() {
+    String s = "";
+
+    for (int i = 0; i < archivio.size(); i++) {
+      Articolo articolo;
+      // estraggo la articolo in posizione i
+      articolo = archivio.get(i);
+
+      // aggiungo la articolo alla stringa
+      s += articolo.toString();
+
+      // dopo aver aggiunto un articolo dobbiamo inserire un ritorno a capo
+      s += "\r\n";
+    }
+    return s;
+  }
+
+  // Metodo che salva i dati nel file archivio.txt
+  public void salvaSuFile() {
+    FileWriter out;
+    try {
+      out = new FileWriter("archivio.txt");
+
+      // Ci facciamo dare dal metodo toString la stringa che rappresenta tutto
+      //l'archivio
+      String sArchivio;
+      sArchivio = toString();
+
+      // Scrivo nel file la stringa ricevuta
+      out.write(sArchivio);
+
+      // Chiudo il file
+      out.close();
+
+    } catch (IOException ex) {
+      Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+  private ArrayList<Articolo> leggiDaFile() {
+    //creo un array list di articoli vuoto
+    ArrayList<Articolo> articoli;
+    articoli = new ArrayList(0);
+
+    FileReader fileReader;
+    try {
+      fileReader = new FileReader("archivio.txt");
+
+      // Creo un oggetto BufferedReader, in quanto contiene il metodo
+      // readLine(), passandogli l'oggetto FileReader creato prima
+      BufferedReader in;
+      in = new BufferedReader(fileReader);
+
+      String linea;
+      String campi[];
+      while ((linea = in.readLine()) != null) {
+        campi = linea.split(";");
+        //campi[0] --> nome
+        //campi[1] --> codice
+        //campi[2] --> prezzo 
+        //campi[3] --> descrizione
+        //campi[4] --> colore
+        float prezzo = Float.parseFloat(campi[2]);
+
+        // Adesso ho i dati necessari per costruite un oggetto
+        // Articolo
+        Articolo articolo = new Articolo(campi[0], campi[1], prezzo, campi[3], campi[4]);
+
+        // aggiungo l'articolo all'ArrayList
+        articoli.add(articolo);
+      }
+
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+    } catch (IOException ex) {
+      Logger.getLogger(Archivio.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    return articoli;
+  }
 }
--- ArchivioJava3BIT/src/archiviojava3bit/Main.java
+++ ArchivioJava3BIT/src/archiviojava3bit/Main.java
@@ -1,8 +1,11 @@
 package archiviojava3bit;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
- * Si occupa dell'interfaccia utente con un menu.
- * Qui si può usare println
+ * Si occupa dell'interfaccia utente con un menu. Qui si può usare println
+ *
  * @author INSERISCI_NOME
  */
 public class Main {
@@ -11,7 +14,134 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creo il archivio
+        Archivio archivio;
+        archivio = new Archivio();
+
+        // Menu
+        // System.in è la tasiera
+        Scanner tastiera;
+        tastiera = new Scanner(System.in);
+
+        int scelta = 0;
+
+        do {
+            System.out.println("--- Gestione articoli per karate ---");
+            System.out.println("1) Visualizza elenco articoli");
+            System.out.println("2) Inserisci articolo");
+            System.out.println("3) Elimina articolo");
+            System.out.println("4) Cerca articolo in base al prezzo");
+            System.out.println("5) Cerca articolo in base alla descrizione");
+            System.out.println("6) Modifica articolo");
+            System.out.println("7) Esci");
+            System.out.print("Scegli l'operazione (1-7): ");
+
+            // Aspetto la scelta dell'utente
+            scelta = Integer.parseInt(tastiera.nextLine()); //bloccante
+
+            switch (scelta) {
+                case 1:
+                    // Chiedo al archivio l'elenco di tutte le articolo
+                    ArrayList<Articolo> elencoArticoli;
+                    elencoArticoli = archivio.elencoArticoli();
+
+                    visualizzaElencoArticoli(elencoArticoli);
+                    break;
+
+                case 2:
+                    // Inserisci articolo
+                    // 1. Chiedo all'utente i dati del nuovo articolo da inserire         
+                    System.out.print("Inserisci il nome: ");
+                    String nome = tastiera.nextLine();
+                    System.out.print("Inserisci la codice: ");
+                    String codice = tastiera.nextLine();
+                    System.out.print("Inserisci la prezzo: ");
+                    float prezzo = Float.parseFloat(tastiera.nextLine());
+                    System.out.print("Inserisci la descrizione: ");
+                    String descrizione = tastiera.nextLine();
+                    System.out.print("Inserisci la colore: ");
+                    String colore = tastiera.nextLine();
+
+                    // 2. Creo un oggetto articolo con i dati forniti dall'utente
+                    Articolo articolo = new Articolo(nome, codice, prezzo, descrizione, colore);
+
+                    // 3. Aggiungo larticolo nel archivio
+                    if (archivio.aggiungi(articolo)) {
+                        System.out.print("Articolo inserito correttamente");
+                    } else {
+                        System.out.print("Esiste gia un articolo con lo stesso codice !");
+                    }
+                    break;
+
+                case 3:
+                    // 1. Chiedo all'utente il codice dell'articolo da eliminare         
+                    System.out.print("Inserisci il codice: ");
+                    codice = tastiera.nextLine();
+
+                    // 2. Elimino la articolo dal archivio         
+                    if (archivio.elimina(codice)) {
+                        System.out.println("Articolo eliminato correttamente.");
+                    } else {
+                        System.out.println("Eliminazione non riuscita.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Inserisci il prezzo minimo: ");
+                    float prezzoMinimo = Float.parseFloat(tastiera.nextLine());
+                    System.out.print("Inserisci il prezzo massimo: ");
+                    float prezzoMassimo = Float.parseFloat(tastiera.nextLine());
+
+                    elencoArticoli = archivio.cercaArticolo(prezzoMinimo, prezzoMassimo);
+
+                    visualizzaElencoArticoli(elencoArticoli);
+
+                    break;
+
+                case 5:
+                    System.out.print("Inserisci la descrizione: ");
+                    descrizione = tastiera.nextLine();
+
+                    elencoArticoli = archivio.cercaArticoloPerDescrizione(descrizione);
+
+                    visualizzaElencoArticoli(elencoArticoli);
+                    break;
+                case 6:
+                    System.out.print("Inserisci il codice: ");
+                    codice = tastiera.nextLine();
+                    System.out.print("Inserisci la nome: ");
+                    nome = tastiera.nextLine();
+                    System.out.print("Inserisci la prezzo: ");
+                    prezzo = Integer.parseInt(tastiera.nextLine());
+                    System.out.print("Inserisci la descrizione: ");
+                    descrizione = tastiera.nextLine();
+                    System.out.print("Inserisci il colore: ");
+                    colore = tastiera.nextLine();
+
+                    if (archivio.modificaArticolo(nome, codice, prezzo, descrizione, colore)) {
+                        System.out.println("Modifica moto avvenuta correttamente.");
+                    } else {
+                        System.out.println("Non è stato possibile modificare la moto.");
+                    }
+                    break;
+            }
+
+        } while (scelta != 7);
+
     }
-    
+
+    private static void visualizzaElencoArticoli(ArrayList<Articolo> elencoArticoli) {
+        System.out.println("N°\tNome\tCodice\tPrezzo\tDescrizione\t\t\tColore");
+        for (int i = 0; i < elencoArticoli.size(); i++) {
+            System.out.print(i + 1);
+            System.out.print("\t" + elencoArticoli.get(i).getNome());
+            System.out.print("\t" + elencoArticoli.get(i).getCodice());
+            System.out.println("\t\t" + elencoArticoli.get(i).getPrezzo());
+            System.out.print("\t" + elencoArticoli.get(i).getDescrizione());
+            System.out.print("\t\t\t" + elencoArticoli.get(i).getColore());
+        }
+
+        System.out.println("\n");
+
+    }
 }
